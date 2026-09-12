7c9e8b225caff2ba28b381d8f294c4e6c5770af9
karambafuze-max <karambafuze@gmail.com>
2026-09-12 19:31:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-self.addEventListener('install',function(){self.skipWaiting()});
-self.addEventListener('activate',function(event){event.waitUntil((async function(){try{var keys=await caches.keys();await Promise.all(keys.map(function(k){return caches.delete(k)}));var reg=await self.registration;await reg.unregister()}catch(e){}var clientsList=await self.clients.matchAll({type:'window'});clientsList.forEach(function(c){c.navigate(c.url)})})())});
-self.addEventListener('fetch',function(){});
+self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
