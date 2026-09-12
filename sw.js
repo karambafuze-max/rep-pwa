1f5b1d4460950f295d41a8d77c29cb15cbd7af46
karambafuze-max <karambafuze@gmail.com>
2026-09-12 19:01:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
-const CACHE='rep-pwa-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+/* REP v4 intentionally removes the old offline cache to avoid stale Safari builds. */
+self.addEventListener('install', function(){ self.skipWaiting(); });
+self.addEventListener('activate', function(event){
+  event.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){return k.indexOf('rep-pwa-')===0;}).map(function(k){return caches.delete(k);}));
+  }).then(function(){return self.registration.unregister();}).then(function(){return self.clients.claim();}));
+});
+self.addEventListener('fetch', function(){});
